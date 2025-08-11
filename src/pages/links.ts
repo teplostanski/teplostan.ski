3707a44ee303b3a4ae0359c65c6df13ec8c0526a
teplostanski <igor@teplostan.ski>
2025-08-11 18:16:59 +0300

    add new links

--- src/pages/links.ts
+++ src/pages/links.ts
@@ -28,9 +28,14 @@ export const links = {
     link: 'mailto:igor@teplostan.ski?bcc=teplostanski@yandex.ru',
   },
   github: { label: 'github', link: 'https://github.com/teplostanski' },
+  codeberg: { label: 'codeberg', link: 'https://codeberg.org/teplostanski' },
   telegram: { label: 'telegram', link: 'https://t.me/teplostanski' },
   bsky: {
     label: 'bsky',
     link: 'https://bsky.app/profile/teplostan.ski',
+  },
+  keyoxide: {
+    label: 'keyoxide',
+    link: 'https://keyoxide.org/hkp/5628A324816DF3C202799215ABE46FF36CB6A99B',
   },
 } as const
